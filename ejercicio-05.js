a621561c14db5c1f8bfb7e4972342f402b5bbf0b
Gemma Batlle <gembb15@gmail.com>
2022-08-05 11:37:06 +0200

    openBootcamp-js-session5

---
+++ ejercicio-05.js
@@ -0,0 +1,19 @@
+
+let alturaCm = 170;
+let alturaM = 1.70;
+let pesoK = 65.6;
+
+let alturaRounded = Math.round(alturaM);
+let pesoTruncated = Math.trunc(pesoK);
+
+console.log(alturaRounded);
+console.log(pesoTruncated);
+
+let max_valor_JS = Number.MAX_VALUE;
+let max_valor_JS1 = max_valor_JS + 1;
+let isTheSame = max_valor_JS === max_valor_JS1;
+
+console.log(max_valor_JS);
+console.log(max_valor_JS1);
+console.log(isTheSame);
+
